7115210c14bc9316c459a3dccbdb98dee490fa12
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-04-10 09:48:39 +0545

    front products route corrected

--- expressDemo/api/controllers/front/products.controller.js
+++ expressDemo/api/controllers/front/products.controller.js
@@ -1,8 +1,89 @@
+const { Product } = require("../../models");
+const { errorHandle, notFoundError } = require("../../lib");
+const { Types } = require("mongoose");
+
 class ProductController {
-  featured = async (req, res, next) => {};
-  latest = async (req, res, next) => {};
-  top = async (req, res, next) => {};
-  byId = async (req, res, next) => {};
+  featured = async (req, res, next) => {
+    try {
+      const products = await Product.find({
+        status: true,
+        featured: true,
+      }).limit(4);
+      if (products) {
+        res.send(products);
+      } else {
+        return notFoundError("Products", next);
+      }
+    } catch (error) {
+      return errorHandle(error, next);
+    }
+  };
+  latest = async (req, res, next) => {
+    try {
+      const products = await Product.find({
+        status: true,
+        featured: true,
+      }).sort({
+        createdAt: "desc",
+      });
+      if (products) {
+        res.send(products);
+      } else {
+        return notFoundError("Products", next);
+      }
+    } catch (error) {
+      return errorHandle(error, next);
+    }
+  };
+  top = async (req, res, next) => {
+    try {
+      const featured = await Product.aggregate()
+        .match({ status: true })
+        .lookup({
+          from: "orderdetails",
+          localField: "_id",
+          foreignField: "productId",
+          as: "details",
+        })
+
+        .addFields({ details: { $size: "$details" } })
+        .sort({ details: "desc" })
+        .limit(4);
+
+      res.send(featured);
+    } catch (err) {
+      return errorHandle(err, next);
+    }
+  };
+  byId = async (req, res, next) => {
+    try {
+      const product = await Product.aggregate()
+        .match({ status: true, _id: new Types.ObjectId(req.params.id) })
+        .lookup({
+          from: "reviews",
+          localField: "_id",
+          foreignField: "productId",
+          as: "reviews",
+        })
+        .lookup({
+          from: "brands",
+          localField: "brandId",
+          foreignField: "brandsId",
+          as: "brand",
+        });
+
+      if (product.length > 0) {
+        let data = product[0];
+        data.brand = data.brand[0];
+
+        res.send(data);
+      } else {
+        return notFoundError("Product", next);
+      }
+    } catch (err) {
+      return errorHandle(err, next);
+    }
+  };
   similar = async (req, res, next) => {};
   review = async (req, res, next) => {};
   search = async (req, res, next) => {};
--- expressDemo/api/routes/front/products.routes.js
+++ expressDemo/api/routes/front/products.routes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { Front } = require("../../controllers");
+const { auth } = require("../../lib");
 
 const router = express.Router();
 
 router.get("/featured", Front.ProductController.featured);
 router.get("/latest", Front.ProductController.latest);
 router.get("/top", Front.ProductController.top);
-router.get("/byId", Front.ProductController.byId);
-router.get("/similar", Front.ProductController.similar);
-router.get("/review", Front.ProductController.review);
 router.get("/search", Front.ProductController.search);
+router.get("/:id", Front.ProductController.byId);
+router.get("/:id/similar", Front.ProductController.similar);
+router.post("/:id/review", auth, Front.ProductController.review);
 
 module.exports = router;
