42f8be05d98e4429843cab4bff201317cafa4bf0
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-05 21:45:42 +0545

    PageNotFound Handled

---
+++ expressDemo/cms-ui/src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="mx-auto my-5">
+      <h1 className="text-center text-bg-danger p-4">404!! Page Not Found</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
--- expressDemo/cms-ui/src/routes/CmsRoutes.jsx
+++ expressDemo/cms-ui/src/routes/CmsRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "@/components";
 import * as Pages from "../pages";
+import PageNotFound from "../pages/PageNotFound";
 
 export const CmsRoutes = () => {
   return (
@@ -9,6 +10,7 @@ export const CmsRoutes = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Pages.Dashboard.List />}></Route>
           <Route path="login" element={<Pages.Auth.Login />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
