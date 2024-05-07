803b3fed149f4c410e49b3223b551e3ec723774d
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-07 23:39:38 +0545

    private route created

--- expressDemo/cms-ui/src/routes/CmsRoutes.jsx
+++ expressDemo/cms-ui/src/routes/CmsRoutes.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "@/components";
 import * as Pages from "../pages";
 import PageNotFound from "../pages/PageNotFound";
+import { PrivateRoutes } from "./PrivateRoutes";
 
 export const CmsRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Pages.Dashboard.List />}></Route>
+          <Route
+            index
+            element={<PrivateRoutes element={<Pages.Dashboard.List />} />}
+          ></Route>
           <Route path="login" element={<Pages.Auth.Login />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Route>
