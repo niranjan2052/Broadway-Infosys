da1a97f6eabf6d0bb8a97282b725086b22d79ca3
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-08 00:36:30 +0545

    success message and request midware integrated

--- expressDemo/cms-ui/src/http/index.js
+++ expressDemo/cms-ui/src/http/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import { fromStorage } from "../lib";
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -10,13 +11,29 @@ const http = axios.create({
 });
 
 http.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    if ("message" in response?.data) {
+      toast.success(response.data.message);
+    }
+    return response;
+  },
   (error) => {
     if ("message" in error?.response?.data) {
       toast.error(error.response.data.message);
     }
     return Promise.reject(error);
   }
+);
+
+http.interceptors.request.use(
+  (config) => {
+    const token = fromStorage("mern");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default http;
