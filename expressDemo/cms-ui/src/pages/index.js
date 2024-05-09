83a249279885f93f98b4a9a48670f32d8b1f9aa3
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-09 18:39:52 +0545

    pages export

--- expressDemo/cms-ui/src/pages/index.js
+++ expressDemo/cms-ui/src/pages/index.js
@@ -1,5 +1,6 @@
 import * as Dashboard from "./dashboard";
 import * as Auth from "./auth";
 import * as Profile from "./profile";
+import * as Staffs from "./staffs";
 
-export { Dashboard, Auth, Profile };
+export { Dashboard, Auth, Profile, Staffs };
