8cc25c6590bce9e3bb7eaaf4c7436ac4d5e11f6a
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-16 11:37:59 +0545

    documentation added to the page

--- expressDemo/cms-ui/src/routes/CmsRoutes.jsx
+++ expressDemo/cms-ui/src/routes/CmsRoutes.jsx
@@ -10,16 +10,20 @@ export const CmsRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          //Links for Dashboard
           <Route
             index
             element={<PrivateRoutes element={<Pages.Dashboard.List />} />}
           ></Route>
+          //For Populating Data
           <Route
             path="/populate"
             element={
               <PrivateRoutes element={<Pages.Dashboard.DataPopulate />} />
             }
           ></Route>
+          //_______________________________________________________________
+          //Links for Profile
           <Route
             path="/profile/edit"
             element={<PrivateRoutes element={<Pages.Profile.Edit />} />}
@@ -28,6 +32,8 @@ export const CmsRoutes = () => {
             path="/profile/password"
             element={<PrivateRoutes element={<Pages.Profile.Password />} />}
           ></Route>
+          //----------------------------------------------------------------
+          //Links for Staffs
           <Route
             path="/staffs"
             element={
@@ -38,6 +44,16 @@ export const CmsRoutes = () => {
             <Route path="create" element={<Pages.Staffs.Create />} />
             <Route path="edit/:id" element={<Pages.Staffs.Edit />} />
           </Route>
+          //Links for Customer
+          <Route
+            path="/customers"
+            element={<PrivateRoutes element={<Outlet />} />}
+          >
+            <Route index element={<Pages.Customer.List />} />
+            <Route path="create" element={<Pages.Customer.Create />} />
+            <Route path="edit/:id" element={<Pages.Customer.Edit />} />
+          </Route>
+          //Links for login Page
           <Route path="login" element={<Pages.Auth.Login />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Route>
