c966567455bc61135195a8d2b9c7ab6f177e63cc
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-16 11:37:43 +0545

    customer exported through pages

--- expressDemo/cms-ui/src/pages/index.js
+++ expressDemo/cms-ui/src/pages/index.js
@@ -2,5 +2,6 @@ import * as Dashboard from "./dashboard";
 import * as Auth from "./auth";
 import * as Profile from "./profile";
 import * as Staffs from "./staffs";
+import * as Customer from "./customer";
 
-export { Dashboard, Auth, Profile, Staffs };
+export { Dashboard, Auth, Profile, Staffs, Customer };
