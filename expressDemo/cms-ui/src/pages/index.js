b2c6b3681d8b745e30e554577cb5c7ff23d2dd9f
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-17 12:44:16 +0545

    links for newly created pages added

--- expressDemo/cms-ui/src/pages/index.js
+++ expressDemo/cms-ui/src/pages/index.js
@@ -2,6 +2,9 @@ import * as Dashboard from "./dashboard";
 import * as Auth from "./auth";
 import * as Profile from "./profile";
 import * as Staffs from "./staffs";
-import * as Customer from "./customer";
+import * as Customers from "./customers";
+import * as Brands from "./brands";
+import * as Products from "./products";
+import * as Categories from "./categories";
 
-export { Dashboard, Auth, Profile, Staffs, Customer };
+export { Dashboard, Auth, Profile, Staffs, Products, Customers, Brands, Categories };
