304ac6a515b3fec9783b838527553c292718ac35
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-24 19:02:16 +0545

    form input exported

--- expressDemo/front-ui/src/components/index.js
+++ expressDemo/front-ui/src/components/index.js
@@ -4,5 +4,14 @@ import { Loading } from "./Loading";
 import { DataTable } from "./DataTable";
 import { ProductSection } from "./ProductSection";
 import { ProductCard } from "./ProductCard";
+import { FormInput } from "./FormInput";
 
-export { Layout, SubmitBtn, Loading, DataTable, ProductSection, ProductCard };
+export {
+  Layout,
+  SubmitBtn,
+  Loading,
+  DataTable,
+  ProductSection,
+  ProductCard,
+  FormInput,
+};
