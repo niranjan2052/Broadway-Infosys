612d7ec52b13c39c0729b121f0a2d25a556f84fa
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-05-24 19:01:03 +0545

    FormInput added to front-ui

---
+++ expressDemo/front-ui/src/components/FormInput.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+export const FormInput = ({
+  formik,
+  label,
+  type = "text",
+  name,
+  placeholder,
+  required = false,
+  as,
+}) => {
+  return (
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor={name}>{label}</Form.Label>
+      <Form.Control
+        type={!as ? type : undefined}
+        name={name}
+        id={name}
+        as={as}
+        value={type != "password" ? formik.values[name] : undefined}
+        required={required}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        placeholder={placeholder}
+        isInvalid={formik.touched[name] && formik.errors[name]}
+      />
+
+      {formik.touched[name] && formik.errors[name] && (
+        <Form.Control.Feedback type="invalid">
+          {formik.errors[name]}
+        </Form.Control.Feedback>
+      )}
+    </Form.Group>
+  );
+};
